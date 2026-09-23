c5a0dbcda9b603a22e75c1efd75d7416619bdc83
Jaell <jaellandreic@gmail.com>
2026-09-23 21:44:55 +0800

    Initialize gomez/issue-List-all-apps

---
+++ Gateway/Areas/Admin/Controllers/TenantAppsController.cs
@@ -0,0 +1,31 @@
+﻿using Gateway.Areas.Admin.Models;
+using ITELECTIVE_SSO.Data;
+using ITElectiveSSO.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gateway.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TenantAppsController : Controller
+    {
+        private readonly SsoDbContext _context;
+
+        public TenantAppsController(SsoDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET /Admin/TenantApps
+        public async Task<IActionResult> Index()
+        {
+            var apps = await _context.TenantApps
+                .OrderBy(a => a.Name)
+                .ToListAsync();
+
+            return View(apps);
+        }
+
+        // TASKS 3-9
+    }
+}
